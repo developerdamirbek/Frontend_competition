66eb86d5a034e6da98ca6e036d4f777b13f3b208
devdamirbek@gmail.com <developerdamirbek@mail.ru>
2024-02-25 13:39:32 +0500

    new change

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs
 var swiper = new Swiper(".swiper", {
     direction: "horizontal",
     loop: true,
-    slidesPerView: 4, // Display 4 slides per view
+    slidesPerView: 4, 
 
     pagination: {
         el: ".swiper-pagination",
