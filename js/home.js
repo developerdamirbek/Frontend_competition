b6ce52eb07c3cd71ea0278027e4961954af72097
devdamirbek@gmail.com <developerdamirbek@mail.ru>
2024-02-25 18:44:29 +0500

    finish

--- js/home.js
+++ js/home.js
@@ -1,8 +1,6 @@
 const modalka = document.querySelector(".modalka")
 const btn = document.querySelector(".hero-btn-2")
 const modal_wrapper = document.querySelector(".bg-modal")
-console.log(modalka);
-console.log(btn);
 
 
 btn.addEventListener("click", () => {
