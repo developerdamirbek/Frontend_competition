b2114ab8c0c7dd89fce5ef8a0a8a8571323f9ce6
devdamirbek@gmail.com <developerdamirbek@mail.ru>
2024-02-25 12:54:48 +0500

    new chnage

---
+++ js/main.js
@@ -0,0 +1,20 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
+
+var swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: 4, // Display 4 slides per view
+
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+});
