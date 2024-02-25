58958392b834d4b8e35c1000e305e1dbea0cc9ab
devdamirbek@gmail.com <developerdamirbek@mail.ru>
2024-02-25 17:58:26 +0500

    new

--- js/about.js
+++ js/about.js
@@ -1,24 +1,15 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
-function initializeSwiper() {
-    let slidesPerViewValue = 1;
-
-    if (window.innerWidth <= 420) {
-        slidesPerViewValue = 1;
-    }
-
-    var swiper = new Swiper(".swiper", {
-        direction: "horizontal",
-        loop: true,
-        slidesPerView: slidesPerViewValue,
-        scrollbar: {
-            el: ".swiper-scrollbar",
-        },
-    });
-}
-
-initializeSwiper();
-
-window.addEventListener('resize', function() {
-    initializeSwiper();
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
 });
